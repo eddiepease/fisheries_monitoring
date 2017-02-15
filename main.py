42c25c4b59abdaa994b34ce506590a4d5c66ca40
Eddie Pease <eddiepease1@gmail.com>
2017-02-15 18:32:38 +0000

    initial copy after commit

--- main.py
+++ main.py
@@ -2,92 +2,240 @@
 #code to solve fish problem
 ##########
 
-import numpy as np
-import tensorflow as tf
+import os
+import glob
+import cv2
+import datetime
+import pandas as pd
+import time
+import warnings
 
-from read_data import load_saved_normalised_train_data,load_saved_normalised_test_data
-from model import create_model
-from helper_code import create_random_batch,create_validation_set,save_model,create_submission
+#warnings.filterwarnings("ignore")
 
-np.random.seed(2016)
-
-def train(model_folder):
-
-    X_train,y_train,id_train = load_saved_normalised_train_data(saved=True)
-    #X_train,y_train = create_random_batch(X_train,y_train,5000)
-
-    #create valid set
-    X_train, y_train, X_valid, y_valid = create_validation_set(X_train,y_train,valid_pc=0.5)
-
-    # define placeholders for tf model
-    x = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
-    y_true = tf.placeholder(tf.float32, shape=[None, 8])
-    retained_pc = tf.placeholder(tf.float32)
-
-    scores = create_model(x, retained_pc)
-    probs = tf.nn.softmax(scores)
-    tf.add_to_collection('probs', probs)
-
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(scores, y_true)
-    train_step = tf.train.AdamOptimizer(learning_rate=0.005).minimize(cross_entropy)
-
-    true_prediction = tf.equal(tf.argmax(scores, 1), tf.argmax(y_true, 1))
-    accuracy = tf.reduce_mean(tf.cast(true_prediction, tf.float32))
-
-    #calculate logloss
-    logloss = tf.reduce_mean(cross_entropy)
+from sklearn.model_selection import KFold
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping
+from keras.utils import np_utils
+from sklearn.metrics import log_loss
+from keras import __version__ as keras_version
 
 
-    batch_size = 64
-    epochs = 2
-
-    # train the model
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        for epoch in range(epochs):
-            print('----- Epoch', epoch, '-----')
-            n_iter = X_train.shape[0] // batch_size
-            total_accuracy = 0
-            for i in range(n_iter):
-                X_batch, y_batch = create_random_batch(X_train, y_train, batch_size)
-                train_step.run(feed_dict={x: X_batch, y_true: y_batch, retained_pc:0.5})
-                current_accuracy = accuracy.eval(feed_dict={x: X_batch, y_true: y_batch, retained_pc:0.5})
-                total_accuracy += current_accuracy
-
-            print(' Train Accuracy:', total_accuracy / n_iter)
-
-            # calc train loss
-            train_logloss = logloss.eval(feed_dict={x: X_train, y_true: y_train, retained_pc: 1.0})
-            print(' Train Logloss:', train_logloss)
-
-            # calc valid loss
-            test_logloss = logloss.eval(feed_dict={x: X_valid, y_true: y_valid, retained_pc:1.0})
-            print(' Test Logloss:', test_logloss)
+def get_im_cv2(path):
+    img = cv2.imread(path)
+    resized = cv2.resize(img, (32, 32), cv2.INTER_LINEAR)
+    return resized
 
 
-        #save the model with checkpoint
-        save_model(sess, model_folder)
+def load_train():
+    X_train = []
+    X_train_id = []
+    y_train = []
+    start_time = time.time()
 
-        #evaluate model
-        evaluate_model(x,retained_pc,probs)
+    print('Read train images')
+    folders = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
+    for fld in folders:
+        index = folders.index(fld)
+        print('Load folder {} (Index: {})'.format(fld, index))
+        path = os.path.join('..', 'input', 'train', fld, '*.jpg')
+        files = glob.glob(path)
+        for fl in files:
+            flbase = os.path.basename(fl)
+            img = get_im_cv2(fl)
+            X_train.append(img)
+            X_train_id.append(flbase)
+            y_train.append(index)
+
+    print('Read train data time: {} seconds'.format(round(time.time() - start_time, 2)))
+    return X_train, y_train, X_train_id
 
 
-#TODO: work out how to have this as a standalone function
-def evaluate_model(x,retained_pc,probs):
-    X_test, id_test = load_saved_normalised_test_data(saved=True)
-    test_probs = probs.eval(feed_dict={x: X_test, retained_pc: 1.0})
-    create_submission(test_probs,id_test,'first_attempt')
+def load_test():
+    path = os.path.join('..', 'input', 'test_stg1', '*.jpg')
+    files = sorted(glob.glob(path))
+
+    X_test = []
+    X_test_id = []
+    for fl in files:
+        flbase = os.path.basename(fl)
+        img = get_im_cv2(fl)
+        X_test.append(img)
+        X_test_id.append(flbase)
+
+    return X_test, X_test_id
 
 
+def create_submission(predictions, test_id, info):
+    result1 = pd.DataFrame(predictions, columns=['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT'])
+    result1.loc[:, 'image'] = pd.Series(test_id, index=result1.index)
+    now = datetime.datetime.now()
+    sub_file = 'submission_' + info + '_' + str(now.strftime("%Y-%m-%d-%H-%M")) + '.csv'
+    result1.to_csv(sub_file, index=False)
 
-if __name__ == "__main__":
 
-    model_folder = 'saved_model/'
-    np.random.seed(2016)
+def read_and_normalize_train_data():
+    train_data, train_target, train_id = load_train()
 
-    train(model_folder)
+    print('Convert to numpy...')
+    train_data = np.array(train_data, dtype=np.uint8)
+    train_target = np.array(train_target, dtype=np.uint8)
 
-    #TODO: set up visualation via tensorboard
-    #TODO: introduce seed
+    print('Reshape...')
+    train_data = train_data.transpose((0, 3, 1, 2))
+
+    print('Convert to float...')
+    train_data = train_data.astype('float32')
+    train_data = train_data / 255
+    train_target = np_utils.to_categorical(train_target, 8)
+
+    print('Train shape:', train_data.shape)
+    print(train_data.shape[0], 'train samples')
+    return train_data, train_target, train_id
+
+
+def read_and_normalize_test_data():
+    start_time = time.time()
+    test_data, test_id = load_test()
+
+    test_data = np.array(test_data, dtype=np.uint8)
+    test_data = test_data.transpose((0, 3, 1, 2))
+
+    test_data = test_data.astype('float32')
+    test_data = test_data / 255
+
+    print('Test shape:', test_data.shape)
+    print(test_data.shape[0], 'test samples')
+    print('Read and process test data time: {} seconds'.format(round(time.time() - start_time, 2)))
+    return test_data, test_id
+
+
+def dict_to_list(d):
+    ret = []
+    for i in d.items():
+        ret.append(i[1])
+    return ret
+
+
+def merge_several_folds_mean(data, nfolds):
+    a = np.array(data[0])
+    for i in range(1, nfolds):
+        a += np.array(data[i])
+    a /= nfolds
+    return a.tolist()
+
+
+def create_model():
+    model = Sequential()
+    model.add(ZeroPadding2D((1, 1), input_shape=(3, 32, 32), dim_ordering='th'))
+    model.add(Convolution2D(4, 3, 3, activation='relu', dim_ordering='th'))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='th'))
+    model.add(Convolution2D(4, 3, 3, activation='relu', dim_ordering='th'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering='th'))
+
+    model.add(ZeroPadding2D((1, 1), dim_ordering='th'))
+    model.add(Convolution2D(8, 3, 3, activation='relu', dim_ordering='th'))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='th'))
+    model.add(Convolution2D(8, 3, 3, activation='relu', dim_ordering='th'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering='th'))
+
+    model.add(Flatten())
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(8, activation='softmax'))
+
+    sgd = SGD(lr=1e-2, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy')
+
+    return model
+
+
+def get_validation_predictions(train_data, predictions_valid):
+    pv = []
+    for i in range(len(train_data)):
+        pv.append(predictions_valid[i])
+    return pv
+
+
+def run_cross_validation_create_models(nfolds=10):
+    # input image dimensions
+    batch_size = 16
+    nb_epoch = 30
+    random_state = 51
+
+    train_data, train_target, train_id = read_and_normalize_train_data()
+
+    yfull_train = dict()
+    kf = KFold(len(train_id), n_folds=nfolds, shuffle=True, random_state=random_state)
+    num_fold = 0
+    sum_score = 0
+    models = []
+    for train_index, test_index in kf:
+        model = create_model()
+        X_train = train_data[train_index]
+        Y_train = train_target[train_index]
+        X_valid = train_data[test_index]
+        Y_valid = train_target[test_index]
+
+        num_fold += 1
+        print('Start KFold number {} from {}'.format(num_fold, nfolds))
+        print('Split train: ', len(X_train), len(Y_train))
+        print('Split valid: ', len(X_valid), len(Y_valid))
+
+        callbacks = [
+            EarlyStopping(monitor='val_loss', patience=3, verbose=0),
+        ]
+        model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+                  shuffle=True, verbose=2, validation_data=(X_valid, Y_valid),
+                  callbacks=callbacks)
+
+        predictions_valid = model.predict(X_valid.astype('float32'), batch_size=batch_size, verbose=2)
+        score = log_loss(Y_valid, predictions_valid)
+        print('Score log_loss: ', score)
+        sum_score += score * len(test_index)
+
+        # Store valid predictions
+        for i in range(len(test_index)):
+            yfull_train[test_index[i]] = predictions_valid[i]
+
+        models.append(model)
+
+    score = sum_score / len(train_data)
+    print("Log_loss train independent avg: ", score)
+
+    info_string = 'loss_' + str(score) + '_folds_' + str(nfolds) + '_ep_' + str(nb_epoch)
+    return info_string, models
+
+
+def run_cross_validation_process_test(info_string, models):
+    batch_size = 16
+    num_fold = 0
+    yfull_test = []
+    test_id = []
+    nfolds = len(models)
+
+    for i in range(nfolds):
+        model = models[i]
+        num_fold += 1
+        print('Start KFold number {} from {}'.format(num_fold, nfolds))
+        test_data, test_id = read_and_normalize_test_data()
+        test_prediction = model.predict(test_data, batch_size=batch_size, verbose=2)
+        yfull_test.append(test_prediction)
+
+    test_res = merge_several_folds_mean(yfull_test, nfolds)
+    info_string = 'loss_' + info_string \
+                  + '_folds_' + str(nfolds)
+    create_submission(test_res, test_id, info_string)
+
+
+if __name__ == '__main__':
+    print('Keras version: {}'.format(keras_version))
+    num_folds = 3
+    info_string, models = run_cross_validation_create_models(num_folds)
+    run_cross_validation_process_test(info_string, models)
 
 
