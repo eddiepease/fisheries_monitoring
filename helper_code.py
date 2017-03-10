5884cee2200e0c0a274e6d382b72f224985236dc
Jonathan Smith <Jonathan@Jonathans-MacBook-Pro-2.local>
2017-03-10 22:44:19 +0000

    Make minor tidyup of helper_code

--- helper_code.py
+++ helper_code.py
@@ -33,11 +33,13 @@ def create_submission(predictions, test_id, info):
     sub_file = 'results/submission_' + info + '_' + str(now.strftime("%Y-%m-%d-%H-%M")) + '.csv'
     result1.to_csv(sub_file, index=False)
 
+
 def dict_to_list(d):
     ret = []
     for i in d.items():
         ret.append(i[1])
     return ret
+
 
 def merge_several_folds_mean(data, nfolds):
     a = np.array(data[0])
